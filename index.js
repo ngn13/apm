3470813fb2ba9562d62b9cb87b2a6d7c79ee35a8
ngn <ngn13proton@proton.me>
2023-01-01 04:06:10 +0300

    added commands

--- index.js
+++ index.js
@@ -1,80 +1,45 @@
 #!/usr/bin/env node
 
 // importing main APM module
+import Cmd from "./lib/cmd.js"
 import APM from "./lib/apm.js"
+import Logger from "./lib/logger.js"
 
-// for cli stuff
-import chalk from "chalk"
-import inquirer from "inquirer"
-
-const welcome = "Awesome Password Manager - APM v1.0.0"
-console.log(chalk.bold(welcome))
-console.log(chalk.bold("_".repeat(welcome.length) + "\n"))
+const cmd = new Cmd()
 const apm = new APM()
+const log = new Logger()
 
-class Logger{
-	error(text){
-		console.log(chalk.red.bold("✗ ") + text)
+const args = process.argv.slice(2)
+if(args.length===0){
+	cmd.help(apm, log)
+	process.exit()
+}
+
+log.inp.question("Matser password: ", (master)=>{
+	process.stdout.write("\n")
+	runCmd(master)
+})
+log.std.muted = true
+
+function runCmd(master){
+	apm.setMaster(master)
+	
+	if(args[0] in cmd.funcMap){	
+		let func = cmd.funcMap[args[0]]
+		
+		if (args.length < func[1]){
+			log.error("Not enough arguments for: " + func[0].name)
+			process.exit()
+		}
+
+		if(args.length > func[1]){
+			log.error("Too mant arguments for: " + func[0].name)
+			process.exit()
+		}
+		func[0](apm, log, args.slice(1))
+
+	}else{
+		log.error("Command not found: " + args[0])
+		process.exit()
 	}
-
-	success(text){
-		console.log(chalk.green.bold("✓ ") + text)
-	}
 }
-
-const logger = new Logger()
-
-// ask for master password
-async function askMaster(){
-	const master = await inquirer.prompt({
-		name: "masterPass",
-		type: "password",
-		message: "Enter master password"
-	})
-
-	apm.setMaster(master.masterPass)
-}
-
-// ask for action
-async function askAction(){
-	const answer = await inquirer.prompt({
-		name: "action",
-		type: "list",
-		message: "Choose an action",
-		choices: [
-			"Add an entry",
-			"Remove an entry",
-			"List entries",
-			"Generate password",
-			"Exit"
-		]
-	})
-
-	switch(answer.action) {
-  		case "Add an entry":
-  		  	//await addEntry()
-  		  	break
-  		case "Remove an entry":
-  		  	//await removeEntry()
-  		  	break
-  		case "List entries":
-  			//await listEntries()
-  			break
-  		case "Generate password":
-  			//await genPass()
-  			break
-  		case "Exit":
-  			process.exit()
-  			break
-  		default:
-  			logger.error("Unknown option")
-  	}
-}
-
-async function ask(){
-	await askMaster()
-	await askAction()
-}
-
-ask()
-
